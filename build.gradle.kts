70e3e85727a0a611e4bd113d253c1c5b64b8ef6b
Lynx <141365347+iLynxcat@users.noreply.github.com>
2025-02-20 10:45:46 -0600

    Switch to Gradle project from Maven

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	id("java")
+	id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "org.modernbeta.admintoolbox"
+version = "0.1"
+
+repositories {
+	mavenCentral()
+	maven("https://repo.papermc.io/repository/maven-public/") {
+		name = "papermc"
+	}
+	maven("https://oss.sonatype.org/content/groups/public/") {
+		name = "sonatype"
+	}
+}
+
+dependencies {
+	compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+}
+
+java {
+	toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.build {
+	dependsOn("shadowJar")
+}
+
+tasks.processResources {
+	val props = mapOf("version" to version)
+	inputs.properties(props)
+	filteringCharset = "UTF-8"
+	filesMatching("plugin.yml") {
+		expand(props)
+	}
+}
