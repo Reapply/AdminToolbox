2daf5db5510f9b0227e932717332ffcd81093ff8
Lynx <lynx@ilynxcat.me>
2025-05-12 23:57:26 -0500

    Remove TODO in favor of #30

--- src/main/java/org/modernbeta/admintoolbox/managers/admin/AdminManager.java
+++ src/main/java/org/modernbeta/admintoolbox/managers/admin/AdminManager.java
@@ -121,8 +121,6 @@ public class AdminManager implements Listener {
 				});
 			});
 		}, () -> {
-			// TODO: gracefully handle this situation. this will execute on logout when
-			// TODO:	the player's original location is unloaded.
 			throw new RuntimeException("Tried to restore a retired player! This is a bug.");
 		});
 	}
